2c21fadb5b233dd31b4d616fca401ae189cfbcfe
David Falcão <david.falcao@jumpseller.com>
2020-09-30 11:26:36 +0100

    update parser whois.nic.co

--- lib/whois/parsers/whois.nic.co.rb
+++ lib/whois/parsers/whois.nic.co.rb
@@ -7,7 +7,7 @@
 #++
 
 
-require_relative 'base_shared2'
+require_relative 'base'
 
 
 module Whois
@@ -18,8 +18,47 @@ module Whois
     # @see Whois::Parsers::Example
     #   The Example parser for the list of all available methods.
     #
-    class WhoisNicCo < BaseShared2
-    end
+    class WhoisNicCo < Base
 
+      property_supported :status do
+        if available?
+          :available
+        else
+          :registered
+        end
+      end
+
+      property_supported :available? do
+        !!(content_for_scanner =~ /No Data Found\n/)
+      end
+
+      property_supported :registered? do
+        !available?
+      end
+
+      property_supported :created_on do
+        if content_for_scanner =~ /Creation Date.?:\s+([^\r\n]+)/
+          Time.parse($1)
+        end
+      end
+
+      property_supported :updated_on do
+        if content_for_scanner =~ /Updated Date.?:\s+([^\r\n]+)/
+          Time.parse($1)
+        end
+      end
+
+      property_supported :expires_on do
+        if content_for_scanner =~ /Registry Expiry Date.?:\s+([^\r\n]+)/
+          Time.parse($1)
+        end
+      end
+
+      property_supported :nameservers do
+        content_for_scanner.scan(/Name Server: (([a-zA-Z0-9]+|[a-zA-Z0-9]*\*[a-zA-Z0-9]*)(\.[a-zA-Z0-9]+){2,3})/).map do |name|
+          Parser::Nameserver.new(:name => name[0])
+        end
+      end
+    end
   end
 end
