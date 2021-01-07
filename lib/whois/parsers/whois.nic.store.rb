22232a65a6ff9e2d50e458b25ed550aba029ab35
David Falcão <david.falcao@jumpseller.com>
2021-01-07 10:43:23 +0000

    Fix .store

--- lib/whois/parsers/whois.nic.store.rb
+++ lib/whois/parsers/whois.nic.store.rb
@@ -21,7 +21,7 @@ module Whois
     class WhoisNicStore < BaseIcannCompliant
 
       self.scanner = Scanners::BaseIcannCompliant, {
-          pattern_available: /^Domain not found\./
+          pattern_available: /^DOMAIN NOT FOUND\./
       }
 
 
